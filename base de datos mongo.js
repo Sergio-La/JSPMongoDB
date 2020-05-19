b916f8bc7abbfbff99fca3e3e434c1775678c195
Sergio-La <64379814+Sergio-La@users.noreply.github.com>
2020-05-19 00:00:08 -0500

    Update base de datos mongo.js

--- base de datos mongo.js
+++ base de datos mongo.js
@@ -9,3 +9,8 @@ db.estudiantes.insert([{codigo: 1010,nombre: 'Laura',apellido: 'Peña'},
 					   {codigo: 1020,nombre: 'Manuel',apellido: 'Muños'},
 					   {codigo: 1030,nombre: 'Andrea',apellido: 'Jarava'},
 					   {codigo: 1040,nombre: 'Camilo',apellido: 'Torres'}]);
+
+//Insertando en material
+db.materias.insert([{codigo: '2010',materia: 'Matematicas',estcod: '1010', estnom:'Laura', nota: '3.5'},
+					{codigo: '2010',materia: 'Matematicas',estcod: '1020', estnom:'Manuel', nota: '3.7'},
+					{codigo: '2020',materia: 'Fisica',estcod: '1030', estnom:'Andrea', nota: '4.0'}]);
